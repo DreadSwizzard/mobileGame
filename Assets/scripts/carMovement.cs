41664bd8a5e4d480d6ea1e43886f9bd6a713b4dd
Levi Lemke <18.levi.lemke@ksd.org>
2018-02-07 13:41:44 -0800

    working buttons.ll

--- Assets/scripts/carMovement.cs
+++ Assets/scripts/carMovement.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class carMovement : MonoBehaviour {
+	float horizontal;
 	public float speed = 5.0f;
 	bool grounded = false;
 	public float rotationSpeed = 30;
 	// Use this for initialization
 	void Start () {
-		
+		horizontal = Input.GetAxis ("Horizontal");
 	}
 	
 	// Update is called once per frame
@@ -19,14 +20,23 @@ public class carMovement : MonoBehaviour {
 			foo.y = 0;
 			transform.rotation = foo;
 			//horizontal stoors the user input when they press a,d,left,right on a scale from -1 to 1
-			float horizontal = Input.GetAxis ("Horizontal");
+			//float horizontal = Input.GetAxis ("Horizontal");
 
 			//create a push variable that combines the user imput
 			gameObject.GetComponent<Rigidbody2D> ().AddTorque (-horizontal);
 		
 		} 
-	
 	}
+	public void backward()
+	{
+		horizontal = -1;
+	}
+
+	public void forward()
+	{
+		horizontal = +1;
+	}
+
 	public void Grounded(){
 		grounded = true;
 	}
