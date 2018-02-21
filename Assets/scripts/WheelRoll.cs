8259fd648aa256932b2beb5930ace0d95fad0671
Levi Lemke <18.levi.lemke@ksd.org>
2018-02-21 12:20:19 -0800

    motorcycle improved.ll

--- Assets/scripts/WheelRoll.cs
+++ Assets/scripts/WheelRoll.cs
@@ -38,13 +38,13 @@ public class WheelRoll : MonoBehaviour {
 	public void backward()
 	{
 		horizontal = -1;
-		gameObject.GetComponent<Rigidbody2D>().AddTorque(-horizontal*2);
+		gameObject.GetComponent<Rigidbody2D>().AddTorque(-horizontal);
 	}
 
 	public void forward()
 	{
 		horizontal = +1;
-		gameObject.GetComponent<Rigidbody2D>().AddTorque(-horizontal*2);
+		gameObject.GetComponent<Rigidbody2D>().AddTorque(-horizontal);
 	}
 
 
