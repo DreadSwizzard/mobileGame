4cdba4c076c236626fca786b559560b24fbd21d7
Levi Lemke <18.levi.lemke@ksd.org>
2018-02-22 11:57:23 -0800

    motorcycle almost done, truch is now green.ll

---
+++ Assets/scripts/death.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "car") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+
+	}
+}
